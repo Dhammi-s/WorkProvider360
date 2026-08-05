d6688eab3b67630ed56bf4593c74bce979d089af
JASSA DHAMMI <jassadhammi@gmail.com>
2026-08-05 17:10:48 +0530

    Remove Me method from AgencyController
    Removed the Me method from AgencyController.

--- Controllers/AgencyController.cs
+++ Controllers/AgencyController.cs
@@ -37,17 +37,5 @@ public sealed class AgencyController : BaseApiController
         }));
     }
     [HttpGet("mefsdfhggh")]
-    public async Task<ActionResult<ApiResponse<AgencyInfoDto>>> Me(CancellationToken ct)
-    {
-        var agency = await _agencies.GetByIdAsync(CurrentAgencyId, ct);
-        if (agency is null)
-            return NotFound(ApiResponse.Fail("Agency not found."));
-
-        return Ok(ApiResponse<AgencyInfoDto>.Ok(new AgencyInfoDto
-        {
-            AgencyId = agency.AgencyId,
-            AgencyName = agency.AgencyName,
-            Location = agency.Location,
-        }));
-    }
+   
 }
